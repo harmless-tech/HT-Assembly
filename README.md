84c76d7131a87f4dad5dd9f294bc354e504716be
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2020-11-03 14:54:57 -0500

    Cmake stuff

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ A more advanced assembly language.
 This branch is a switch from Rust to C for the compiler and runtime.
 
 ### TODO
+- Testing.
+- Split libs into their own cmake projects.
 
 ##### More to come.
--- src/main.c
+++ src/main.c
@@ -4,7 +4,13 @@
 #include <pthread.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello!");
+	printf("Hello!\n");
+	
+	// Args
+	printf("Arg Amount: %d\n", argc);
+	for(int i = 0; i < argc; i++)
+		printf("Arg %d: %s\n", i, argv[i]);
+	//
 	
 	return 0;
 }
